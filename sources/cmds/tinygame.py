90dbb1369c85d2a8f27cdd1a6c9910b47b51be83
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2020-12-05 23:33:24 +0800

    amend bug and parameter

--- sources/cmds/tinygame.py
+++ sources/cmds/tinygame.py
@@ -9,19 +9,17 @@ from datahook import yamlhook
 
 class Sokoban():
     def __init__(self,difficulty):
-        if (difficulty==None):
+        if difficulty==None :
             high=10
             width=10
             number=1
-        else:
-            high=difficulty*10
-            width=difficulty*10
+        else :
+            high=10 + difficulty
+            width=10 + difficulty
             number=difficulty
-
         self.map1=[[]]*high
         self.box=[]
         repeat=[]
-
         for i in range(high):
             if i == 0 or i == high-1 : self.map1[i] = ["🔲"]*width
             else:
@@ -29,8 +27,7 @@ class Sokoban():
                 for x in range(width) :
                     if x == 0 or x == width-1 :self.map1[i][x] = "🔲"
                     else : self.map1[i][x]="⬛"
-        
-        while(True):
+        while 1 :
             if [ra.randint(1,high-2),ra.randint(1,width-2)] not in repeat :
                 repeat.append([ra.randint(2,high-3),ra.randint(2,width-3)])
             if len(repeat)==2+number:
@@ -43,20 +40,17 @@ class Sokoban():
                 self.end=repeat[1]
             else :
                 self.box.append(repeat[i])
-
     def userinput(self,x):
-        y={"🔼":self.up,"🔽":self.down,"◀":self.letf,"▶":self.right}
-        y[x]()
-
+        if x in ["◀","🔼","🔽","▶","⏹"]:
+            y={"🔼":self.up,"🔽":self.down,"◀":self.left,"▶":self.right}
+            y[x]()
     def up(self):
         if self.map1[self.player[0]-1][self.player[1]] != "🔲" :
-            #自己移動
-            if ([self.player[0]-1,self.player[1]] !=self.end) and ([self.player[0]-1,self.player[1]] not in self.box):
+            if ([self.player[0]-1,self.player[1]] !=self.end) and ([self.player[0]-1,self.player[1]] not in self.box):#自己移動
                 self.player[0]-=1
-            #推箱子
-            elif ([self.player[0]-1,self.player[1]] in self.box) :
+            elif ([self.player[0]-1,self.player[1]] in self.box) :#推箱子
                 box=self.box[self.box.index([self.player[0]-1,self.player[1]])]
-                if self.map1[box[0]-1][box[1]] !="🔲" :
+                if self.map1[box[0]-1][box[1]] !="🔲" and [box[0]-1,box[1]] not in self.box:
                     if [box[0]-1,box[1]] ==self.end:
                         del self.box[self.box.index([self.player[0]-1,self.player[1]])]
                     else :
@@ -65,27 +59,23 @@ class Sokoban():
 
     def down(self):
         if self.map1[self.player[0]+1][self.player[1]] != "🔲" :
-            #自己移動
-            if ([self.player[0]+1,self.player[1]] !=self.end) and ([self.player[0]+1,self.player[1]] not in self.box):
+            if ([self.player[0]+1,self.player[1]] !=self.end) and ([self.player[0]+1,self.player[1]] not in self.box):#自己移動
                 self.player[0]+=1
-            #推箱子
-            elif ([self.player[0]+1,self.player[1]] in self.box) :
+            elif ([self.player[0]+1,self.player[1]] in self.box) :#推箱子
                 box=self.box[self.box.index([self.player[0]+1,self.player[1]])]
-                if self.map1[box[0]+1][box[1]] !="🔲" :
+                if self.map1[box[0]+1][box[1]] !="🔲" and [box[0]+1,box[1]] not in self.box:
                     if [box[0]+1,box[1]] ==self.end:
                         del self.box[self.box.index([self.player[0]+1,self.player[1]])]
                     else :
                         self.box[self.box.index([self.player[0]+1,self.player[1]])][0]+=1
                     self.player[0]+=1
-    def letf(self):
+    def left(self):
         if self.map1[self.player[0]][self.player[1]-1] != "🔲" :
-            #自己移動
-            if ([self.player[0],self.player[1]-1] !=self.end) and ([self.player[0],self.player[1]-1] not in self.box):
+            if ([self.player[0],self.player[1]-1] !=self.end) and ([self.player[0],self.player[1]-1] not in self.box):#自己移動
                 self.player[1]-=1
-            #推箱子
-            elif ([self.player[0],self.player[1]-1] in self.box) :
+            elif ([self.player[0],self.player[1]-1] in self.box) :#推箱子
                 box=self.box[self.box.index([self.player[0],self.player[1]-1])]
-                if self.map1[box[0]][box[1]-1] !="🔲" :
+                if self.map1[box[0]][box[1]-1] !="🔲" and [box[0],box[1]-1] not in self.box:
                     if [box[0],box[1]-1] ==self.end:
                         del self.box[self.box.index([self.player[0],self.player[1]-1])]
                     else :
@@ -93,28 +83,22 @@ class Sokoban():
                     self.player[1]-=1
     def right(self):
         if self.map1[self.player[0]][self.player[1]+1] != "🔲" :
-            #自己移動
-            if ([self.player[0],self.player[1]+1] !=self.end) and ([self.player[0],self.player[1]+1] not in self.box):
+            if ([self.player[0],self.player[1]+1] !=self.end) and ([self.player[0],self.player[1]+1] not in self.box):#自己移動
                 self.player[1]+=1
-            #推箱子
-            elif ([self.player[0],self.player[1]+1] in self.box) :
+            elif ([self.player[0],self.player[1]+1] in self.box) :#推箱子
                 box=self.box[self.box.index([self.player[0],self.player[1]+1])]
-                if self.map1[box[0]][box[1]+1] !="🔲" :
+                if self.map1[box[0]][box[1]+1] !="🔲" and [box[0],box[1]+1] not in self.box:
                     if [box[0],box[1]+1] ==self.end:
                         del self.box[self.box.index([self.player[0],self.player[1]+1])]
                     else :
                         self.box[self.box.index([self.player[0],self.player[1]+1])][1]+=1
                     self.player[1]+=1
-                    
     def mapprint(self):
         map2=copy.deepcopy(self.map1)
-        # 人物
-        map2[self.player[0]][self.player[1]]="🌝"
-        # 終點
-        map2[self.end[0]][self.end[1]]="🟨"
+        map2[self.player[0]][self.player[1]]="🌝"#人物
+        map2[self.end[0]][self.end[1]]="🟨"#終點
         for i in self.box :
-            #箱子
-            map2[i[0]][i[1]]="🔳"
+            map2[i[0]][i[1]]="🔳"#箱子
         if not len(self.box):return ("\n".join(["".join(i) for i in map2]),True)
         else :return ("\n".join(["".join(i) for i in map2]),False)
         
